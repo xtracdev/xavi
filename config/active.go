d84b3d86c9d6a531f60344e411f0fa8b2728a956
d-smith <doug.smith@fmr.com>
2016-06-16 18:01:00 -0700

    Added active config

--- config/active.go
+++ config/active.go
@@ -6,6 +6,9 @@ func init() {
 	activeConfig = make(map[string]*ServiceConfig)
 }
 
+//RecordActiveConfig allows a service config constructed from
+//a listener name to be recorded and made available to all
+//code using the xavi framework
 func RecordActiveConfig(serviceConfig *ServiceConfig) {
 	if serviceConfig == nil || serviceConfig.Listener == nil {
 		return
@@ -14,10 +17,14 @@ func RecordActiveConfig(serviceConfig *ServiceConfig) {
 	activeConfig[serviceConfig.Listener.Name] = serviceConfig
 }
 
+//ActiveConfigForListener returns the ServiceConfig associated with
+//the provided listener names
 func ActiveConfigForListener(listenerName string) *ServiceConfig {
 	return activeConfig[listenerName]
 }
 
+//ActiveListenerNames reurns the names of the listeners for whom
+//the associated ServiceConfig has been recorded.
 func ActiveListenerNames() []string {
 	keys := make([]string, len(activeConfig))
 
